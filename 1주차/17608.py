b44e6d366af49a3d0b8dd8a92d986ba51e6fa0ed
YuBin Kim <43837921+luwbe1@users.noreply.github.com>
2021-09-13 21:21:25 +0900

    Update 17608.py

--- 1주차/17608.py
+++ 1주차/17608.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
  
 n = int(input()) # 막대 입력
 sticks = [int(input()) for i in range(n)] # for문
-max = sticks[-1] #최대 높이
+max = sticks[-1] #최대 높이, 스택의 맨 
 count = 1
  
 for i in range(n): #for문 시작
-    now = sticks.pop() #현재
+    now = sticks.pop() # 하나씩 뽑음
     if max < now: #최대 높이가 현재보다 작으면
         count += 1 #하나 더 카운트
         max = now #최대높이는 현재로 맞춤
