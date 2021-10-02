00b64f80297b607dc8e01e5ef636a7ed212d6e72
YuBin Kim <43837921+luwbe1@users.noreply.github.com>
2021-10-02 22:04:11 +0900

    Update 2959.py

--- 3주차/2959.py
+++ 3주차/2959.py
@@ -1,4 +1,4 @@
-A, B, C, D = map(int, input().split()) # A,B,C,D 입력
+A, B, C, D = map(int, input().split()) # 거북이가 생각한 양의 정수 A,B,C,D 입력
 turtle = [A, B, C, D] # 거북이
 turtle.sort(reverse=True) # 정렬
 print(turtle[1] * turtle[-1]) # 직사각형 넓이 구하기
