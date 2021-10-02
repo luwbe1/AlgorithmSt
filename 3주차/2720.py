3a82bf60d7e03858119a24706b54d4bf0760a74e
YuBin Kim <43837921+luwbe1@users.noreply.github.com>
2021-10-02 14:53:19 +0900

    Create 2720.py

---
+++ 3주차/2720.py
@@ -0,0 +1,14 @@
+import sys
+
+T=int(sys.stdin.readline()) # T 입력
+
+for _ in range(T): 
+    money = int(sys.stdin.readline()) # 돈 
+    a = money // 25 # 25
+    money %=25
+    b = money // 10 # 10
+    money %=10
+    c = money // 5 # 5
+    money %=5
+    d = money
+    print(a,b,c,d)
